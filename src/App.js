8b55ab47c43dff9f58b40040d700df0d94289737
David <dmdedatekk@gmail.com>
2022-01-07 20:49:35 +1100

    React 2.0+ Auth solution

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import {Authenticator} from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
+    <Authenticator>
+      {({ signOut, user })}
     <div className="App">
       <header>
         <p>
           Dedatekk presents ZipIt
         </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Authentication Confirmed</h1>
+        <h1>Welcome, {user.username}</h1>
       </header>
-      <AmplifySignOut />
+      <button onClick={signOut}>Sign out</button>
     </div>
+    </Authenticator>
   );
 }
 
-export default withAuthenticator (App);
+export default App;
