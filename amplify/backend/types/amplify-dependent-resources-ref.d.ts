0d1c997f4d751a58acdf9bc8dfb7a7e782c4830a
David <dmdedatekk@gmail.com>
2022-01-08 04:11:32 +1100

    API

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,22 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "zipit": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3zipitstoraged3gxwxxosmkmkf": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
