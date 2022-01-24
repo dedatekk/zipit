14aa6a990fdff9002ecf1fd002747d6fde393a0b
David <dmdedatekk@gmail.com>
2022-01-24 18:28:45 +1100

    random page formatting

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
 
 import Header from './components/Header/Header';
-import List from './components/List/List';
+import Body from './components/Body/Body';
 import Map from './components/Map/Map';
 
 const App = () => {
@@ -11,13 +11,11 @@ const App = () => {
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
-        <Grid item xs={12} md={4}>
-          <List />
-        </Grid>
-        <Grid item xs={12} md={8}>
+        <Grid item xs={12} md={12}>
           <Map />
         </Grid>
       </Grid>
+      <Body />
     </>
   );
 }
